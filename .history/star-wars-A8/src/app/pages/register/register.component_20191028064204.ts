82a5584cbeff8fce0c5c76ee140270066182ef4a
@e_lcargnelutti <@e_lcargnelutti>
2019-10-28 09:58:10 -0300

    se incorpora button y spiner

---
+++ .history/star-wars-A8/src/app/pages/register/register.component_20191028064204.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/shared/forms/custom-validators/custom-validators';
+import { User } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  formGroupRegister: FormGroup;
+  user: User = new User();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formGroupRegister = new FormGroup({
+      name: new FormControl(''),
+      lastName: new FormControl(this.user.lastName),
+      userName: new FormControl(this.user.userName),
+      pass: new FormControl(this.user.pass, Validators.compose([CustomValidators.number,
+      CustomValidators.minlength(4),
+      CustomValidators.maxlength(6)]))
+    });
+  }
+
+  registerUser() {
+  }
+
+}
---
+++ .history/star-wars-A8/src/app/services/user.service_20191028080430.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StarshipsService {
+
+  constructor( private http: HttpClient) { }
+
+
+  createUser(user: User): Observable<any> {
+    const subject = new Subject<any>();
+    
+
+
+
+    return subject.asObservable();
+
+  }
+
+  getAllUsers() {}
+
+  getUserById() {}
+
+
+  private setUser(user) {
+    localStorage.users = JSON.stringify()
+  }
+}
